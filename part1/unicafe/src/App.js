954c54960b41ccef05edb7f7a38e03eef17a445a
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2021-09-06 22:45:21 +0600

    Completed excercises upto 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,47 @@
 import React, { useState } from 'react'
 
-const Display = ({good,neutral,bad}) => {
-  console.log(good, neutral, bad)
+const StatisticLine = ({text, value}) => {
+    if (text === "Positive") {
+      return(
+        <tr>
+          <td>{text}</td>
+          <td>{value} %</td>
+        </tr>
+      )
+    }
+
+    return(
+      <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+    )
+}
+
+
+
+const Statistics = ({good,neutral,bad}) => {
+  const total = good+neutral+bad
+  const avg = (good-bad)/total
+  const positivePercentage = (100/total)*good
+
+  if (total === 0) {
+    return(
+      <p>No feedback given</p>
+    )
+  }
+
   return(
-    <div>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-    </div>
+    <table>
+      <tbody>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="All" value={total} />
+      <StatisticLine text="Average" value={avg} />
+      <StatisticLine text="Positive" value={positivePercentage} />
+      </tbody>
+    </table>
   )
 }
 
@@ -30,11 +63,12 @@ const App = () => {
 
   return (
     <div>
-      <Display good={good} neutral={neutral} bad={bad} />
       <h1>Give Feedback</h1>
-      <Button text="Good" onclick={() => updateGood} />
-      <Button text="Neutral" onclick={() => updateNeutral} />
-      <Button text="Bad" onclick={() => updateBad} />
+      <Button text="Good" onclick={updateGood} />
+      <Button text="Neutral" onclick={updateNeutral} />
+      <Button text="Bad" onclick={updateBad} />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
       
     </div>
   )
