42288e3c66ab0ddd17aea4ff2f038bf81d8a0ea5
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2021-10-03 02:04:02 +0600

    test commit

---
+++ part3/index.js
@@ -0,0 +1,45 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+const persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    const responseText = `<p>Phone has info for ${persons.length} people</p><p>${date}</p>`
+    response.send(responseText)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+})
