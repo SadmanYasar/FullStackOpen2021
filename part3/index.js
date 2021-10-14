1c8e6db322c77a8f51b3d84ce47e474174600a20
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2021-10-14 22:40:44 +0600

    changed port

--- part3/index.js
+++ part3/index.js
@@ -107,7 +107,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
 })
