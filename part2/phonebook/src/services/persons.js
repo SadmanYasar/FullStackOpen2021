2df8c61d18f11e6d50c70bac8a0bf7d18f7e4a4d
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2021-10-14 22:19:22 +0600

    update url in frontend

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const url = 'http://localhost:3001/persons'
+const url = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
     const request = axios.get(url)
--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,16 @@
-const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
-const persons = [
+app.use(cors())
+app.use(express.json())
+
+morgan.token('request-body', (request, response) => JSON.stringify(request.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
+
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -25,6 +33,20 @@ const persons = [
     }
 ]
 
+const getRandomInt = () => {
+  const min = 1
+  const max = 10000
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const generateId = () => {
+  const id = persons.length > 0
+  ? getRandomInt()
+  : 0
+  
+  return id
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -33,11 +55,57 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const match = persons.find(person => id === person.id)
+
+  if (match) {
+    response.json(match)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
 app.get('/info', (request, response) => {
     const date = new Date()
     const responseText = `<p>Phone has info for ${persons.length} people</p><p>${date}</p>`
     response.send(responseText)
 })
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name || !body.number) {
+    response.status(400).json({error: "missing field(s)"})
+    return
+  }
+
+  const match = persons.find(person => person.name === body.name)
+
+  if ( match !== undefined ) {
+    response.status(409).json({error: "name must be unique"})
+    return
+  }
+
+  const Person = {...body, id: generateId()}
+  persons = persons.concat(Person)
+
+  response.json(Person)
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => id !== person.id)
+  response.status(204).end() 
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
