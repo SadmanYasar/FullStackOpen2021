07f792659f06abcf4c1e9d5bf4fe410b0511c376
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-01-26 18:09:49 +0600

    finished 6.20

--- part6/anecdote/src/components/AnecdoteForm.js
+++ part6/anecdote/src/components/AnecdoteForm.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
-const AnecdoteForm = () => {
-    const dispatch = useDispatch()
-
+const AnecdoteForm = (props) => {
     const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
-        dispatch(setNotification(`${content} has been added`, 5))
+        props.createAnecdote(content)
+        props.setNotification(`${content} has been added`, 5)
     }
 
     return(
@@ -25,4 +23,16 @@ const AnecdoteForm = () => {
     )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = dispatch => {
+    return {
+        createAnecdote: (content) => dispatch(createAnecdote(content)),
+        setNotification: (content, timeInSeconds) => dispatch(
+            setNotification(content, timeInSeconds)
+        )
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(AnecdoteForm)
--- part6/anecdote/src/components/Filter.js
+++ part6/anecdote/src/components/Filter.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { filterChange } from '../reducers/filterReducer'
 
-const Filter = () => {
-  const dispatch = useDispatch()
-
+const Filter = (props) => {
   const handleChange = (event) => {
     const filter = event.target.value
-    dispatch(filterChange(filter))
+    props.filterChange(filter)
   }
+  
   const style = {
     marginBottom: 10
   }
@@ -20,4 +19,15 @@ const Filter = () => {
   )
 }
 
-export default Filter
+const mapDispatchToProps = dispatch => {
+  return {
+    filterChange: filter => {
+      dispatch(filterChange(filter))
+    }
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Filter)
