83333727756cb59979d9c8a0ccb01b401d05423d
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-02-10 21:12:56 +0600

    added chakra ui

--- part7/bloglist/src/index.js
+++ part7/bloglist/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import store from './utils/store'
 
@@ -14,7 +15,9 @@ export const history = createBrowserHistory()
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Router>
   </Provider>,
   document.getElementById('root')
