870bc76ac8b349e58f9de160b53ee0b7e3d9b81a
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-02-19 02:25:58 +0600

    finished 9.4

--- part9/first-part/index.ts
+++ part9/first-part/index.ts
@@ -1,1 +1,12 @@
-console.log('hmmm');
+import express from 'express';
+const app = express();
+
+app.get('/hello', (_req, res) => {
+    res.send('Hello fullstack!');
+})
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
