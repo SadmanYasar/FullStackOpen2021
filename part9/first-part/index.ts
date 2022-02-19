fe92a218a98d3ac4bccf0ef6cb9a1724d0536eda
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-02-20 00:40:52 +0600

    finished 9.7

--- part9/first-part/index.ts
+++ part9/first-part/index.ts
@@ -1,27 +1,30 @@
 import express from 'express';
 import { parseBmiArguments, calculateBmi } from './bmiCalculator';
+import { parseExcerciseArguments, calculateExercises } from './exerciseCalculator';
 
 const app = express();
 
-app.get('/hello', (_req, res) => {
-    res.send('Hello fullstack!');
+app.use(express.json());
+
+app.get('/hello', (_request, response) => {
+    response.send('Hello fullstack!');
 });
 
-app.get('/bmi', (req, res) => {
-    const query = req.query;
+app.get('/bmi', (request, response) => {
+    const query = request.query;
 
     if (!query.height || !query.mass) {
-        return res.status(400).send({ error: 'malformatted parameters' });
+        return response.status(400).send({ error: 'malformatted parameters' });
     }
 
     try {
-        const {height, mass} = parseBmiArguments(
+        const { height, mass } = parseBmiArguments(
             Number(query.height), 
             Number(query.mass)
         );
 
         const result = calculateBmi(height, mass);
-        return res.status(200).send({
+        return response.status(200).send({
             mass,
             height,
             bmi: result
@@ -29,10 +32,38 @@ app.get('/bmi', (req, res) => {
         
     } catch (error: unknown) {
         if(error instanceof Error) {
-            return res.status(400).send({ error: error.message });  
+            return response.status(400).send({ error: error.message });  
         }
 
-        return res.status(400).send({error: 'Could not handle request'});
+        return response.status(400).send({error: 'Could not handle requestuest'});
+    }
+});
+
+app.post('/exercises', (request, response) => {
+    console.log(request.body);
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
+    const dailyExercises = request.body.daily_exercises;
+
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
+    const dailyTarget = request.body.target;
+
+    if (!dailyExercises || !dailyTarget) {
+        return response.status(400).send({ error: 'parameters missing' });
+    } else {
+        try {
+            const { target, dailyExerciseHours } = parseExcerciseArguments(
+                /* eslint-disable-next-line @typescript-eslint/no-unsafe-argument */
+                dailyTarget, dailyExercises
+            );
+    
+            return response.status(200).send(calculateExercises(target, dailyExerciseHours));
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                return response.status(400).send({ error: e.message });
+            }
+    
+            return response.status(400).send({ error: 'Could not handle request' });
+        }
     }
 });
 
