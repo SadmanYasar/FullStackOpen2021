b66d32123e3f1b6c1a749b6e29a879d9e95ba042
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-02-20 23:11:18 +0600

    finished 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 const app = express();
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (_request, response) => {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.get('/api/ping', (_request, response) => {
     return response.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`server running on  port ${PORT}`);
 });
